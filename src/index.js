30441647f4d3c5adfec2f67e3d5696dc48238e1a
Nathan Burgo <nthnbrg185@gmail.com>
2018-07-14 22:43:46 -0600

    Began adding websockets functionality for multiple users

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import { createStore } from 'redux'
 
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { addUser } from './actions'
 import chat from './reducers/index'
 
 const store = createStore(chat)
+
+store.dispatch(addUser('Me'))
 
 ReactDOM.render(
   <Provider store={store}>
